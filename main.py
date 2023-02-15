fada12957abc2dc61fe919e9271275402ea29858
Glumov Andrey <gluman@yandex.ru>
2023-02-15 22:41:02 +0300

    18.02.2023 v1.1

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ from _ast import keyword
 import lxml
 import requests
 from bs4 import BeautifulSoup
+from fake_headers import Headers
 
-template = '''
-<div class="ip" id="d_clip_button" style="cursor: pointer;">
-                                    <span>93.109.99.66</span>
-
-                                    <i class="ip-icon-shape btn-copy"></i>
-                                </div>
-'''
 # URL 'https://hh.ru/search/vacancy?text=python&area=1&area=2'
 HOST = 'https://hh.ru'
 
