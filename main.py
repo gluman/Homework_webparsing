0a7ddd58bd124b21a3643794d9169fca785ea7c3
Glumov Andrey <gluman@yandex.ru>
2023-02-19 22:07:49 +0300

    19.03.2023 v1.1

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 ##
-from _ast import keyword
+# 1 Необходимо парсить страницу со свежими вакансиями с поиском по "Python" и городами "Москва" и "Санкт-Петербург". Эти параметры задаются по ссылке
+# 2 Нужно выбрать те вакансии, у которых в описании есть ключевые слова "Django" и "Flask".
+# 3 Записать в json информацию о каждой вакансии - ссылка, вилка зп, название компании, город.
+
+
+
+# from _ast import keyword
 import lxml
+import re
 import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
+import json
 keywords = ['Django', 'Flask']
-
-
-HOST = 'https://hh.ru/search/vacancy?text=python&area=1&area=2'
+HOST = 'https://spb.hh.ru/search/vacancy?text=Python+Django+Flask&salary=&area=1&area=2&ored_clusters=true&enable_snippets=true'
 
 def get_headers():
     headers = Headers(browser='firefox', os='win')
@@ -17,14 +23,29 @@ response = requests.get(HOST, headers=get_headers())
 hh_main = response.text
 
 soup = BeautifulSoup(hh_main, features='lxml')
-main_content = soup.find('div', id='ally-main-content')
-vacances = main_content.findAll('div', class_='vacancy-serp-item__layout')
+main_content = soup.find('div', id="a11y-main-content")
+vacanceses = main_content.findAll('div', class_='vacancy-serp-item__layout')
 
+parced = []
 
-for vanvancy in vacances:
-    description = vanvancy.find('div', ='vacancy-serp__vacancy_snippet_requirement')
-    link
-    compensation
-    company_name
-    city
-print(span.text)
+for vacancy in vacanceses:
+    info = vacancy.find('div', class_='g-user-content').findAll('div', class_='bloko-text')
+    desc = []
+    for item in info:
+        desc.append(item.text)
+
+    if set(keywords).issubset(desc):
+        link = vacancy.find('a')['href']
+        compensation = vacancy.find('span', class_='bloko-header-section-3')
+        company_name = vacancy.find('a', class_='bloko-link bloko-link_kind-tertiary')
+        city = vacancy.find('div', class_='bloko-text')
+        item ={
+            'link': link,
+            'compensation': compensation,
+            'company_name': company_name,
+            'city': city
+        }
+        parced.append(item)
+
+with open('parsed.json', 'w') as file:
+    json.dump(parced, file, indent=5)
